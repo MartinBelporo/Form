97cbd596a70a5f6c57c1b213ae72475024630cb9
crispin <criskkan@gmail.com>
2022-06-01 13:43:56 -0400

    Premiers code

--- code.py
+++ code.py
@@ -1,4 +1,8 @@
 # This program prints Hello, world!
 
+from traceback import print_last
+
+
 print('Hello, world!')
-print ('Hello bro, world')
+print('Hello bro, world')
+print('thank you for this opportunity')
